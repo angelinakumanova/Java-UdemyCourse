4969c1b9286ab5ddf03ce58c462b54b2858b24ce
Angelina Kumanova <angelinakumanova@gmail.com>
2023-12-11 22:16:04 +0200

    Added

---
+++ ClassesObjectsAndMethods/MethodExerciseFive.java
@@ -0,0 +1,15 @@
+package ClassesObjectsAndMethods;
+
+public class MethodExerciseFive {
+    public static void main(String[] args) {
+        // Write a method that allows you to pass any number of Strings as parameter inputs without
+        // using an array
+        method("one", "two", "three");
+        method("22", "133", "3232", " 1123", "3131312");
+
+    }
+
+    public static void method (String...args) {
+
+    }
+}
