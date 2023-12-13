673d54740cdbf31b092f878f57b241b639b728df
Angelina Kumanova <angelinakumanova@gmail.com>
2023-12-13 22:23:42 +0200

    Added

---
+++ WorkingWithText/Ex3.java
@@ -0,0 +1,13 @@
+package WorkingWithText;
+
+
+public class Ex3 {
+    public static void main(String[] args) {
+        String input = "   Hi, my name is Angie!      ";
+
+        input = input.trim();
+        
+        System.out.println(input);
+    }
+
+}
