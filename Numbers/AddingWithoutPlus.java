18385761be95ff1b3ac18d0ba10000039c514cbc
Angelina Kumanova <angelinakumanova@gmail.com>
2023-12-22 02:52:33 +0200

    Added

---
+++ Numbers/AddingWithoutPlus.java
@@ -0,0 +1,8 @@
+package Numbers;
+
+public class AddingWithoutPlus {
+    public static void main(String[] args) {
+        byte result = 2 | 4;
+        System.out.println("Addition result: " + result);
+    }
+}
