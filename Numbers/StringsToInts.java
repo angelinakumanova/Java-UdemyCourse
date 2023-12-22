06b15f7d480d9f76c6529d2eb798311ce387ce84
Angelina Kumanova <angelinakumanova@gmail.com>
2023-12-22 20:12:25 +0200

    Added

---
+++ Numbers/StringsToInts.java
@@ -0,0 +1,11 @@
+package Numbers;
+
+public class StringsToInts {
+    public static void main(String[] args) {
+        String input1 = "37";
+        String input2 = "13";
+
+        // Printing the result
+        System.out.println(Integer.parseInt(input1) + Integer.parseInt(input2));
+    }
+}
