44e3d0db59740ce9d412e02b77be3ba7dbe4eb48
Angelina Kumanova <angelinakumanova@gmail.com>
2024-01-02 01:01:45 +0200

    Added

---
+++ ControlFlow/DaysOfTheWeekII.java
@@ -0,0 +1,24 @@
+package ControlFlow;
+
+public class DaysOfTheWeekII {
+    public static void main(String[] args) {
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        int count = 0;
+
+        while (count < days.length) {
+            System.out.print(days[count] + " ");
+            count++;
+        }
+
+        System.out.println();
+
+        // capitalized
+        count = 0;
+        while (count < days.length) {
+            String day = count % 2 == 0 ? days[count] : days[count].toUpperCase();
+            System.out.print(day + " ");
+            count++;
+        }
+    }
+}
